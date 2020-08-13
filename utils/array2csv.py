189e46d3241f8c5f076746b68fc822daf1f8ae16
anilsathyan <anilsathyan7@gmail.com>
2020-08-13 18:01:10 +0530

    data conversion utils

---
+++ utils/array2csv.py
@@ -0,0 +1,19 @@
+
+'''Convert numpy array data into csv format'''
+
+import numpy as np
+
+def array2csv(puzzle, solution):
+
+  # Load the entire numpy array into memory
+  puzzle=np.load(puzzle).reshape(-1,81)
+  solution=np.load(solution).reshape(-1,81)
+   
+  # Convert array elements into a list of strings
+  puzlist=["".join(item) for item in puzzle.astype(str)]
+  sollist=["".join(item) for item in solution.astype(str)]
+
+  # Save combined data in csv format
+  np.savetxt('sudoku.csv', np.column_stack((puzlist, sollist)), delimiter=',', fmt='%s', header='quizzes,solutions', comments='')
+
+array2csv(puzzle='test_puzzles_2.npy',solution='test_solutions_2.npy')
---
+++ utils/csv2array.py
@@ -0,0 +1,26 @@
+'''Convert csv data into numpy array format'''
+
+import numpy as np
+
+puzzle, solution = [], []
+data = np.genfromtxt('sudoku.csv',dtype='S81',delimiter=',', skip_header=1)
+
+# Extract and save the puzzle characters as numpy arrays 
+for row in data:
+   
+   puz = np.frombuffer(row[0], np.int8) - 48
+   puz = puz.reshape((9,9))
+   puzzle.append(puz)
+   
+np.save('puzzle.npy', np.array(puzzle))
+
+del puzzle
+
+# Extract and save the solution characters as numpy arrays  
+for row in data:
+   
+   sol = np.frombuffer(row[1], np.int8) - 48
+   sol = sol.reshape((9,9))
+   solution.append(sol)
+
+np.save('solution.npy', np.array(solution))
